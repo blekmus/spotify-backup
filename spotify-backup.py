c5b90b74ed37f679cc9d66a398b9ad82550542d9
Walker <caidenwilson@protonmail.com>
2021-08-06 14:40:26 +0530

    folder structure change // foreign playlists

--- spotify-backup.py
+++ spotify-backup.py
@@ -147,6 +147,28 @@ class SpotifyAPI:
         def __init__(self, access_token):
             self.access_token = access_token
 
+# simple recursive y/n input with default
+def yesno(question, default=None):
+    ans = input(question).strip().lower()
+
+    if default is not None:
+        if ans == '':
+            if default == 'y':
+                return True
+            return False
+        elif ans not in ['y', 'n']:
+            print(f'{ans} is invalid, please try again...')
+            return yesno(question)
+        if ans == 'y':
+            return True
+    else:
+        if ans not in ['y', 'n']:
+            print(f'{ans} is invalid, please try again...')
+            return yesno(question)
+        if ans == 'y':
+            return True
+
+    return False
 
 # return formatted hh mm ss
 def timematter(x):
@@ -189,22 +211,26 @@ def save_track(filename, track_list):
 
     # loop through tracks and add them as rows
     for track in track_list:
-        writer.writerow({
-            'Track ID': track['track']['id'],
-            'Album ID': track['track']['album']['id'],
-            'Track Name': track['track']['name'],
-            'Album Name': track['track']['album']['name'],
-            'Album Tracks': track['track']['album']['total_tracks'],
-            'Artist Name(s)': ", ".join([artist['name'] for artist in track['track']['artists']]),
-            'Release Date': track['track']['album']['release_date'],
-            'Duration (ms)': timematter(int(track['track']['duration_ms']) / 1000),
-            'Explicity': track['track']['explicit'],
-            'Album Type': track['track']['album']['album_type'],
-            'Popularity': track['track']['popularity'],
-            'Added On': track['added_at'],
-            'Track URL': track['track']['external_urls']['spotify'],
-            'Album URL': track['track']['album']['external_urls']['spotify']
-        })
+        try:
+            writer.writerow({
+                'Track ID': track['track']['id'],
+                'Album ID': track['track']['album']['id'],
+                'Track Name': track['track']['name'],
+                'Album Name': track['track']['album']['name'],
+                'Album Tracks': track['track']['album']['total_tracks'],
+                'Artist Name(s)': ", ".join([artist['name'] for artist in track['track']['artists']]),
+                'Release Date': track['track']['album']['release_date'],
+                'Duration (ms)': timematter(int(track['track']['duration_ms']) / 1000),
+                'Explicity': track['track']['explicit'],
+                'Album Type': track['track']['album']['album_type'],
+                'Popularity': track['track']['popularity'],
+                'Added On': track['added_at'],
+                'Track URL': track['track']['external_urls']['spotify'],
+                'Album URL': track['track']['album']['external_urls']['spotify']
+            })
+        except KeyError:
+            logging.error(f"Failed to load track {track['track']['name']}")
+            continue
 
     file.close()
 
@@ -228,14 +254,18 @@ def save_artist(filename, artist_list):
 
     # loop through artists and add them as rows
     for artist in artist_list:
-        writer.writerow({
-            'ID': artist['id'],
-            'Name': artist['name'],
-            'Type': artist['type'],
-            'Followers': artist['followers']['total'],
-            'Popularity': artist['popularity'],
-            'URL': artist['external_urls']['spotify'],
-        })
+        try:
+            writer.writerow({
+                'ID': artist['id'],
+                'Name': artist['name'],
+                'Type': artist['type'],
+                'Followers': artist['followers']['total'],
+                'Popularity': artist['popularity'],
+                'URL': artist['external_urls']['spotify'],
+            })
+        except KeyError:
+            logging.error(f"Failed to load artist {artist['name']}")
+            continue
 
     file.close()
 
@@ -263,18 +293,22 @@ def save_album(filename, album_list):
 
     # loop through albums and add them as rows
     for album in album_list:
-        writer.writerow({
-            'ID': album['album']['id'],
-            'Name': album['album']['name'],
-            'Tracks': album['album']['total_tracks'],
-            'Artist Name(s)': ", ".join([album['name'] for album in album['album']['artists']]),
-            'Release Date': album['album']['release_date'],
-            'Label': album['album']['label'],
-            'Type': album['album']['album_type'],
-            'Popularity': album['album']['popularity'],
-            'Added On': album['added_at'],
-            'URL': album['album']['external_urls']['spotify']
-        })
+        try:
+            writer.writerow({
+                'ID': album['album']['id'],
+                'Name': album['album']['name'],
+                'Tracks': album['album']['total_tracks'],
+                'Artist Name(s)': ", ".join([album['name'] for album in album['album']['artists']]),
+                'Release Date': album['album']['release_date'],
+                'Label': album['album']['label'],
+                'Type': album['album']['album_type'],
+                'Popularity': album['album']['popularity'],
+                'Added On': album['added_at'],
+                'URL': album['album']['external_urls']['spotify']
+            })
+        except KeyError:
+            logging.error(f"Failed to load album {album['album']['name']}")
+            continue
 
     file.close()
 
@@ -301,17 +335,21 @@ def save_podcast(filename, podcast_list):
 
     # loop through podcases and add them as rows
     for podcast in podcast_list:
-        writer.writerow({
-            'ID': podcast['show']['id'],
-            'Name': podcast['show']['name'],
-            'Description': podcast['show']['description'],
-            'Episodes': podcast['show']['total_episodes'],
-            'Publisher': podcast['show']['publisher'],
-            'Type': podcast['show']['media_type'],
-            'Explicity': podcast['show']['explicit'],
-            'Added On': podcast['added_at'],
-            'URL': podcast['show']['external_urls']['spotify']
-        })
+        try:
+            writer.writerow({
+                'ID': podcast['show']['id'],
+                'Name': podcast['show']['name'],
+                'Description': podcast['show']['description'],
+                'Episodes': podcast['show']['total_episodes'],
+                'Publisher': podcast['show']['publisher'],
+                'Type': podcast['show']['media_type'],
+                'Explicity': podcast['show']['explicit'],
+                'Added On': podcast['added_at'],
+                'URL': podcast['show']['external_urls']['spotify']
+            })
+        except KeyError:
+            logging.error(f"Failed to load podcast {podcast['show']['name']}")
+            continue
 
     file.close()
 
@@ -342,27 +380,34 @@ def save_episode(filename, episode_list):
 
     # loop through episodes and add them as rows
     for episode in episode_list:
-        writer.writerow({
-            'Episode ID': episode['episode']['id'],
-            'Show ID': episode['episode']['show']['id'],
-            'Episode Name': episode['episode']['name'],
-            'Show Name': episode['episode']['show']['name'],
-            'Publisher': episode['episode']['show']['publisher'],
-            'Description': episode['episode']['description'],
-            'Release Date': episode['episode']['release_date'],
-            'Duration (ms)': timematter(int(episode['episode']['duration_ms']) / 1000),
-            'Explicity': episode['episode']['explicit'],
-            'Show Type': episode['episode']['show']['media_type'],
-            'Added On': episode['added_at'],
-            'Episode URL': episode['episode']['external_urls']['spotify'],
-            'Show URL': episode['episode']['show']['external_urls']['spotify']
-        })
+        try:
+            writer.writerow({
+                'Episode ID': episode['episode']['id'],
+                'Show ID': episode['episode']['show']['id'],
+                'Episode Name': episode['episode']['name'],
+                'Show Name': episode['episode']['show']['name'],
+                'Publisher': episode['episode']['show']['publisher'],
+                'Description': episode['episode']['description'],
+                'Release Date': episode['episode']['release_date'],
+                'Duration (ms)': timematter(int(episode['episode']['duration_ms']) / 1000),
+                'Explicity': episode['episode']['explicit'],
+                'Show Type': episode['episode']['show']['media_type'],
+                'Added On': episode['added_at'],
+                'Episode URL': episode['episode']['external_urls']['spotify'],
+                'Show URL': episode['episode']['show']['external_urls']['spotify']
+            })
+        except KeyError:
+            logging.error(f"Failed to load episode {episode['episode']['name']}")
+            continue
 
     file.close()
 
 
 # log into the Spotify API.
 spotify = SpotifyAPI.authorize(
+    # id from spotify client app created at
+    # https://developer.spotify.com/dashboard/applications
+    # it has http://127.0.0.1:43019/redirect as the redirect URI
     client_id="fc84b0b659d64f568f72d0d6009ad965",
     scope="playlist-read-private playlist-read-collaborative user-library-read user-follow-read",
 )
@@ -374,55 +419,78 @@ me = spotify.get('me')
 logging.info(f"Logged in as {me['display_name']} ({me['id']})")
 
 
+# for playlists not owned by user
+save_foreign_playlists = yesno('Save playlists not owned by you? [y/N]: ', 'n')
+
+
 # create needed dirs
 logging.info('Creating needed directories')
-os.makedirs('./done/Playlists', exist_ok=True)
+os.makedirs('./done/Music/Playlists', exist_ok=True)
+os.makedirs('./done/Podcasts', exist_ok=True)
 
 
 # save liked songs
 logging.info('Loading liked songs...')
 liked_tracks = spotify.list(f"users/{me['id']}/tracks", {'limit': 50})
 logging.info('Saving liked songs')
-save_track('done/Liked.csv', liked_tracks)
+save_track('done/Music/Liked.csv', liked_tracks)
 
 
-# playlist data
-logging.info('Loading playlists...')
-playlist_data = spotify.list('users/{user_id}/playlists'.format(user_id=me['id']), {'limit': 50})
+# get all playlist data
+playlist_data = spotify.list(f"users/{me['id']}/playlists", {'limit': 50})
+
+
+# get user's playlist data
+logging.info("Loading user's playlists...")
 user_playlists = [playlist for playlist in playlist_data if playlist['owner']['id'] == me['id']]
-logging.info(f'Found {len(user_playlists)} playlists')
+logging.info(f"Found {len(user_playlists)} user's playlists")
 
-# saving playlist songs
+# saving user's playlist songs
 for playlist in user_playlists:
-    logging.info(f"Loading playlist: {playlist['name']} ({playlist['tracks']['total']} songs)")
+    logging.info(f"Loading user playlist: {playlist['name']} ({playlist['tracks']['total']} songs)")
     playlist_tracks = spotify.list(playlist['tracks']['href'], {'limit': 100})
     logging.info(f"Saving {playlist['name']}'s songs")
-    save_track(f"done/Playlists/{playlist['name']}.csv", playlist_tracks)
+    save_track(f"done/Music/Playlists/{playlist['name']} - {playlist['id']}.csv", playlist_tracks)
 
 
-# following artist data
+# check whether to save foreign playlists
+if save_foreign_playlists:
+    # get foreign playlist data
+    logging.info("Loading foreign playlists...")
+    foreign_playlists = [playlist for playlist in playlist_data if playlist['owner']['id'] != me['id']]
+    logging.info(f"Found {len(foreign_playlists)} foreign playlists")
+
+    # saving foreign playlist songs
+    for playlist in foreign_playlists:
+        logging.info(f"Loading foreign playlist: {playlist['name']} ({playlist['tracks']['total']} songs)")
+        playlist_tracks = spotify.list(playlist['tracks']['href'], {'limit': 100})
+        logging.info(f"Saving {playlist['name']}'s songs")
+        save_track(f"done/Music/Playlists/{playlist['name']} - {playlist['id']}.csv", playlist_tracks)
+
+
+# following artists data
 logging.info('Loading followed artists...')
 following_artist_data = spotify.list_artists('me/following', {'type': 'artist', 'limit': 50})
 logging.info(f'Found {len(following_artist_data)} artists')
-save_artist('done/Artists.csv', following_artist_data)
+save_artist('done/Music/Artists.csv', following_artist_data)
 
 
 # saved album data
 logging.info('Loading saved albums...')
 saved_album_data = spotify.list('me/albums', {'limit': 50})
 logging.info(f'Found {len(saved_album_data)} albums')
-save_album('done/Albums.csv', saved_album_data)
+save_album('done/Music/Albums.csv', saved_album_data)
 
 
-# saved album data
-logging.info('Loading saved podcasts...')
+# saved podcast shows data
+logging.info('Loading saved podcast shows...')
 saved_podcast_data = spotify.list('me/shows', {'limit': 50})
 logging.info(f'Found {len(saved_podcast_data)} podcasts')
-save_podcast('done/Podcasts.csv', saved_podcast_data)
+save_podcast('done/Podcasts/Shows.csv', saved_podcast_data)
 
 
-# save saved podcast episodes
+# saved podcast episode data
 logging.info('Loading saved podcast episodes...')
 saved_episode_data = spotify.list("me/episodes", {'limit': 50})
 logging.info('Saving episodes')
-save_episode('done/Episodes.csv', saved_episode_data)
+save_episode('done/Podcasts/Episodes.csv', saved_episode_data)
