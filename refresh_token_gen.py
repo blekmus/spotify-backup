ab4385aa71286248cfabf11fa75c8f582f194729
blekmus <dinil@protonmail.com>
2026-09-06 15:40:40 -0400

    spotify needs localhost uri to be 127.0.0.1 when on http

--- refresh_token_gen.py
+++ refresh_token_gen.py
@@ -15,7 +15,7 @@ CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 # Server-side
 port = 8888
-redirect_uri = f"http://localhost:8888/callback"
+redirect_uri = f"http://127.0.0.1:8888/callback"
 scope = "playlist-read-private playlist-read-collaborative user-library-read user-follow-read"
 
 
